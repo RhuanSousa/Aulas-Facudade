61ae334c496d465a85a0e8380f0a1830d3b19bad
Rhuan <Rhuansantos.cp@gmail.com>
2024-10-23 21:17:55 -0300

    update

--- 231024/main.c
+++ 231024/main.c
@@ -2,37 +2,71 @@
 
 float menu_float(void){
 
-    float ret;
+    float res;
     printf("\nInsira um numero: ");
-    scanf("%f", &ret);
+    scanf("%f", &res);
 
-    return ret;   
+    return res;   
 }
 
 char menu_principal(void)
 {
-    char opcao;
-    printf("\nEscolha um opcao: \n");
-    printf("+ soma\n");
-    printf("- subtracao\n");
-    printf("* multiplicacao\n");
-    printf("/ divisao\n");
-    scanf("%c", &opcao);
+    while (1)
+    {
+        char opcao;
+        printf("\nEscolha um opcao: \n");
+        printf("+ soma\n");
+        printf("- subtracao\n");
+        printf("* multiplicacao\n");
+        printf("/ divisao\n");
+        scanf("%c", &opcao);
 
-    return opcao;
+        
+        if (opcao == '+' || opcao == '-' || opcao == '*' || opcao == '/')
+        {
+            return opcao;
+        } 
+
+        printf("\nOperacao invalida!\n");
+    }
+}
+
+float soma(float parcela1, float parcela2) {
+
+    float resultado = parcela1 + parcela2;
+
+    return resultado;
 }
 
 int main() {
     char opcao;
+    float res;
+    float num1, num2;
 
     opcao = menu_principal();
 
-    if (opcao == '+' || opcao == '-' || opcao == '*' || opcao == '/')
+    num1 = menu_float();
+    num2 = menu_float();
+
+
+    printf("\nEscolha = %c", opcao);
+
+    switch (opcao)
     {
-        printf("\n escolha = %c", opcao);
-    } else {
-        printf("\n Opcao invalida");
+    case '+':
+        res = soma(num1, num2);
+        break;
+    case '-':
+        break;
+    case '*':
+        break;
+    case '/':
+        break;
+    default:
+        break;
     }
+
+    printf("\n %.1f %c %.1f = %.1f", num1, opcao, num2, res);
 
     return 0;
 }
